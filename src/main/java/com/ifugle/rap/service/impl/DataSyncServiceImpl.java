5410b6e9d711ed9ae30e1a6cabaab4f3dd38ccd7
hlyaowan <hlyaowan@163.com>
2019-10-31 15:38:16 +0800

    修改同步脚本

--- src/main/java/com/ifugle/rap/service/impl/DataSyncServiceImpl.java
+++ src/main/java/com/ifugle/rap/service/impl/DataSyncServiceImpl.java
@@ -146,7 +146,7 @@ public class DataSyncServiceImpl implements DataSyncService {
             insertYhzxXnzzTpcQyForSync();
             //insertXxzxXxmxForSync();
         }
-        //insertBotChatRequestForSync();
+        insertBotChatRequestForSync();
     }
 
 
