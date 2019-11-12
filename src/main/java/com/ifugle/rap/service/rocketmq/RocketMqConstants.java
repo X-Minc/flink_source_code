268c4f27d0a6026d5215313d52ae086f0358b2a7
hlyaowan <hlyaowan@163.com>
2019-11-12 21:14:37 +0800

    修改配置

--- src/main/java/com/ifugle/rap/service/rocketmq/RocketMqConstants.java
+++ src/main/java/com/ifugle/rap/service/rocketmq/RocketMqConstants.java
@@ -18,7 +18,7 @@ public class RocketMqConstants {
 
     public final static String SecretKey  =  System.getProperty("rap.mq.rocket.password","RCHE8NY4DkIldIei3O4ub61FwxEaGa");
 
-    public final static String NameServer = System.getProperty("rap.mq.rocket.server","http://onsaddr.mq-internet-access.mq-internet.aliyuncs.com:80");
+    public final static String NameServer = System.getProperty("rap.mq.rocket.server","http://onsaddr.cn-hangzhou.mq-internal.aliyuncs.com:8080");
 
     public final static String MQ_TOPIC = System.getProperty("rap.mq.rocket.topic","topic_bot_data_sync_test");
 
