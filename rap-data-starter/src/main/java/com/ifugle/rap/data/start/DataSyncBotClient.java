8ca575fd7389b1c3295fa9ad7875c470f2e8c9fd
hlyaowan <hlyaowan@163.com>
2021-04-12 17:57:51 +0800

    增加开关控制丁税宝

---
+++ rap-data-starter/src/main/java/com/ifugle/rap/data/start/DataSyncBotClient.java
@@ -0,0 +1,39 @@
+package com.ifugle.rap.data.start;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ifugle.rap.service.DataSyncBqService;
+import com.ifugle.rap.service.DataSyncService;
+
+/**
+ * @author ifugle
+ * @version $
+ * @since 4月 12, 2021 17:36
+ */
+@Service
+public class DataSyncBotClient {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(DataSyncDsbClient.class);
+
+    @Autowired
+    private DataSyncService dataSyncService;
+
+
+    public void syncBot(){
+        LOGGER.info("Thread excute sync BOT data starting ");
+        while (true) {
+            try {
+                LOGGER.info("Thread excute sync data ");
+                Thread.sleep(10000);
+                //从文件读取初始化运行状态，判断是否已经执行过数据初始化全量更新
+                // 处理不断增加的数据的线程；初始化后的添加数据同步过来，该线程不存在问题。处理正常
+                dataSyncService.dataSyncInsertIncrementData();
+            } catch (Exception e) {
+                LOGGER.error("Thread excute sync data error e = ", e);
+            }
+        }
+    }
+}
